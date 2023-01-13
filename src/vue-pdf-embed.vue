65ff17dadfa28a4e2ab50e0ab61abf32a6874490
Max <max@olibri.us>
2023-01-13 16:38:02 +0100

    try other rendering method

--- src/vue-pdf-embed.vue
+++ src/vue-pdf-embed.vue
@@ -37,20 +37,20 @@
 </template>
 
 <script>
-import * as pdf from 'pdfjs-dist/legacy/build/pdf.js'
-import PdfWorker from 'pdfjs-dist/legacy/build/pdf.worker.js'
-import { PDFLinkService } from 'pdfjs-dist/legacy/web/pdf_viewer.js'
+import * as pdf from "pdfjs-dist/legacy/build/pdf.js";
+import PdfWorker from "pdfjs-dist/legacy/build/pdf.worker.js";
+import { PDFLinkService } from "pdfjs-dist/legacy/web/pdf_viewer.js";
 import {
   // addPrintStyles,
   // createPrintIframe,
   emptyElement,
-  releaseChildCanvases,
-} from './util.js'
+  releaseChildCanvases
+} from "./util.js";
 
-pdf.GlobalWorkerOptions.workerPort = new PdfWorker()
+pdf.GlobalWorkerOptions.workerPort = new PdfWorker();
 
 export default {
-  name: 'VuePdfEmbed',
+  name: "VuePdfEmbed",
   props: {
     /**
      * Whether the annotation layer should be disabled.
@@ -66,7 +66,7 @@ export default {
      * Desired page height.
      * @values Number, String
      */
-    height: [Number, String],
+    height: [ Number, String ],
     /**
      * Component identifier (inherited by page containers with page number
      * postfixes).
@@ -88,13 +88,13 @@ export default {
      * @values Number, String
      */
     rotation: {
-      type: [Number, String],
-      validator(value) {
+      type: [ Number, String ],
+      validator (value) {
         if (value % 90 !== 0) {
-          throw new Error('Rotation must be 0 or a multiple of 90.')
+          throw new Error("Rotation must be 0 or a multiple of 90.");
         }
-        return true
-      },
+        return true;
+      }
     },
     /**
      * Desired ratio of canvas size to document size.
@@ -106,46 +106,46 @@ export default {
      * @values Object, String, URL, TypedArray
      */
     source: {
-      type: [Object, String, URL, Uint8Array],
-      required: true,
+      type: [ Object, String, URL, Uint8Array ],
+      required: true
     },
     /**
      * Desired page width.
      * @values Number, String
      */
-    width: [Number, String],
+    width: [ Number, String ],
     /**
      * Desired margin between pages
      * @values Number
      */
     margin: Number,
     cameraOffsetX: Number,
-    cameraOffsetY: Number,
+    cameraOffsetY: Number
   },
-  data() {
+  data () {
     return {
       document: null,
       pageCount: null,
-      pageNums: [],
-    }
+      pageNums: []
+    };
   },
   computed: {
-    linkService() {
+    linkService () {
       if (!this.document || this.disableAnnotationLayer) {
-        return null
+        return null;
       }
 
-      const service = new PDFLinkService()
-      service.setDocument(this.document)
+      const service = new PDFLinkService();
+      service.setDocument(this.document);
       service.setViewer({
         scrollPageIntoView: ({ pageNumber }) => {
-          this.$emit('internal-link-clicked', pageNumber)
-        },
-      })
-      return service
-    },
+          this.$emit("internal-link-clicked", pageNumber);
+        }
+      });
+      return service;
+    }
   },
-  created() {
+  created () {
     this.$watch(
       () => [
         this.source,
@@ -157,33 +157,33 @@ export default {
         this.width,
         this.scale
       ],
-      async ([newSource], [oldSource]) => {
+      async ([ newSource ], [ oldSource ]) => {
         if (newSource !== oldSource) {
-          console.log('source changed, reset document...', newSource)
-          releaseChildCanvases(this.$el)
-          await this.document?.destroy()
-          await this.load()
+          console.log("source changed, reset document...", newSource);
+          releaseChildCanvases(this.$el);
+          await this.document?.destroy();
+          await this.load();
         }
-        await this.render()
+        await this.render();
       }
-    )
+    );
     this.$watch(
-      () => [this.cameraOffsetX, this.cameraOffsetY],
-      async () => {
-        console.log('offset changed...', this.cameraOffsetX, this.cameraOffsetY)
-        await this.update()
+      () => [ this.cameraOffsetX, this.cameraOffsetY ],
+      () => {
+        console.log("offset changed...", this.cameraOffsetX, this.cameraOffsetY);
+        this.update();
       }
       // TODO : THIS CAUSES FLICKERING AND SHOULD BE OPTIMIZED !
       // async () =>  await this.render()
-    )
+    );
   },
-  async mounted() {
-    await this.load()
-    await this.render()
+  async mounted () {
+    await this.load();
+    await this.render();
   },
-  beforeDestroy() {
-    releaseChildCanvases(this.$el)
-    this.document?.destroy()
+  beforeDestroy () {
+    releaseChildCanvases(this.$el);
+    this.document?.destroy();
   },
   methods: {
     /**
@@ -191,18 +191,18 @@ export default {
      * aspect ratio.
      * @param {number} ratio - Page aspect ratio.
      */
-    getPageDimensions(ratio) {
-      let width, height
+    getPageDimensions (ratio) {
+      let width, height;
 
       if (this.height && !this.width) {
-        height = this.height
-        width = height / ratio
+        height = this.height;
+        width = height / ratio;
       } else {
-        width = this.width || this.$el.clientWidth
-        height = width * ratio
+        width = this.width || this.$el.clientWidth;
+        height = width * ratio;
       }
 
-      return [width, height]
+      return [ width, height ];
     },
     /**
      * Loads a PDF document. Defines a password callback for protected
@@ -210,76 +210,76 @@ export default {
      *
      * NOTE: Ignored if source property is not provided.
      */
-    async load() {
+    async load () {
       if (!this.source) {
-        return
+        return;
       }
 
-      console.log('loading document...', this.source)
+      console.log("loading document...", this.source);
 
       try {
         if (this.source._pdfInfo) {
-          this.document = this.source
+          this.document = this.source;
         } else {
-          const documentLoadingTask = pdf.getDocument(this.source)
+          const documentLoadingTask = pdf.getDocument(this.source);
           documentLoadingTask.onProgress = (progressParams) => {
-            this.$emit('progress', progressParams)
-          }
+            this.$emit("progress", progressParams);
+          };
           documentLoadingTask.onPassword = (callback, reason) => {
-            const retry = reason === pdf.PasswordResponses.INCORRECT_PASSWORD
-            this.$emit('password-requested', callback, retry)
-          }
-          this.document = await documentLoadingTask.promise
+            const retry = reason === pdf.PasswordResponses.INCORRECT_PASSWORD;
+            this.$emit("password-requested", callback, retry);
+          };
+          this.document = await documentLoadingTask.promise;
         }
-        this.pageCount = this.document.numPages
-        console.log('document loaded !')
-        this.$emit('loaded', this.document)
+        this.pageCount = this.document.numPages;
+        console.log("document loaded !");
+        this.$emit("loaded", this.document);
       } catch (e) {
-        this.document = null
-        this.pageCount = null
-        this.pageNums = []
-        this.$emit('loading-failed', e)
+        this.document = null;
+        this.pageCount = null;
+        this.pageNums = [];
+        this.$emit("loading-failed", e);
       }
     },
 
-    async render() {
+    async render () {
       if (!this.document) {
-        return
+        return;
       }
 
       try {
         this.pageNums = this.page
-          ? [this.page]
-          : [...Array(this.document.numPages + 1).keys()].slice(1)
+          ? [ this.page ]
+          : [ ...Array(this.document.numPages + 1).keys() ].slice(1);
 
         await Promise.all(
           this.pageNums.map(async (pageNum, i) => {
-            const page = await this.document.getPage(pageNum)
-            const [canvas, draws, div1, div2] = this.$el.children[i].children
-            const [actualWidth, actualHeight] = this.getPageDimensions(
+            const page = await this.document.getPage(pageNum);
+            const [ canvas, draws, div1, div2 ] = this.$el.children[i].children;
+            const [ actualWidth, actualHeight ] = this.getPageDimensions(
               page.view[3] / page.view[2]
-            )
+            );
 
             if ((this.rotation / 90) % 2) {
-              canvas.style.width = `${Math.floor(actualHeight)}px`
+              canvas.style.width = `${Math.floor(actualHeight)}px`;
               // canvas.style.height = `${Math.floor(actualWidth)}px`
             } else {
-              canvas.style.width = `${Math.floor(actualWidth)}px`
+              canvas.style.width = `${Math.floor(actualWidth)}px`;
               // canvas.style.height = `${Math.floor(actualHeight)}px`
             }
 
             // Propagate the height to the nested canvas
-            draws.style.width = canvas.style.width
+            draws.style.width = canvas.style.width;
             // draws.style.height = canvas.style.height
 
             // set margins
-            canvas.style.margin = `${Math.floor(this.margin)}px`
-            draws.style.margin = `${Math.floor(this.margin)}px`
+            canvas.style.margin = `${Math.floor(this.margin)}px`;
+            draws.style.margin = `${Math.floor(this.margin)}px`;
 
-            await this.renderPage(page, canvas, draws, actualWidth)
+            await this.renderPage(page, canvas, draws, actualWidth);
 
             if (!this.disableTextLayer) {
-              await this.renderPageTextLayer(page, div1, actualWidth)
+              await this.renderPageTextLayer(page, div1, actualWidth);
             }
 
             if (!this.disableAnnotationLayer) {
@@ -287,44 +287,41 @@ export default {
                 page,
                 div2 || div1,
                 actualWidth
-              )
+              );
             }
           })
-        )
+        );
 
-        this.$emit('rendered')
+        this.$emit("rendered");
       } catch (e) {
-        this.document = null
-        this.pageCount = null
-        this.pageNums = []
-        this.$emit('rendering-failed', e)
+        this.document = null;
+        this.pageCount = null;
+        this.pageNums = [];
+        this.$emit("rendering-failed", e);
       }
     },
 
-    async update() {
+    update () {
       if (!this.document) {
-        return
+        return;
       }
 
       try {
         this.pageNums = this.page
-          ? [this.page]
-          : [...Array(this.document.numPages + 1).keys()].slice(1)
+          ? [ this.page ]
+          : [ ...Array(this.document.numPages + 1).keys() ].slice(1);
 
-        console.log('updating...', this.pageNums)
-
-        await Promise.all(
-          this.pageNums.map(async (pageNum, i) => {
-            const page = await this.document.getPage(pageNum)
-            const [canvas, draws] = this.$el.children[i].children
-            this.updatePage(page, canvas, draws)
-          })
-        )
+        console.log("updating...", this.pageNums);
+        this.pageNums.map(async (pageNum, i) => {
+          const page = await this.document.getPage(pageNum);
+          const [ canvas, draws ] = this.$el.children[i].children;
+          this.updatePage(page, canvas, draws);
+        });
       } catch (e) {
-        this.document = null
-        this.pageCount = null
-        this.pageNums = []
-        this.$emit('update-failed', e)
+        this.document = null;
+        this.pageCount = null;
+        this.pageNums = [];
+        this.$emit("update-failed", e);
       }
     },
 
@@ -335,43 +332,62 @@ export default {
      * @param {HTMLCanvasElement} draws - HTML canvas drawings.
      * @param {number} width - Actual page width.
      */
-    async renderPage(page, canvas, draws, width) {
+    async renderPage (page, canvas, draws, width) {
       const viewport = page.getViewport({
-        scale: Math.ceil(width / page.view[2]) + 1,
+        // scale: Math.ceil(width / page.view[2]) + 1,
+        scale: this.scale,
         rotation: this.rotation,
-      })
+        offsetX: this.cameraOffsetX,
+        offsetY: this.cameraOffsetY
+      });
 
-      canvas.width = draws.width = viewport.width
-      canvas.height = draws.height = viewport.height
+      // Support HiDPI-screens.
+      var outputScale = window.devicePixelRatio || 1;
+      canvas.width = Math.floor(viewport.width * outputScale);
+      canvas.height = Math.floor(viewport.height * outputScale);
+      canvas.style.width = Math.floor(viewport.width) + "px";
+      canvas.style.height = Math.floor(viewport.height) + "px";
 
-      const context = canvas.getContext('2d')
-      const contextDraws = draws.getContext('2d')
-      let scale = Math.max(this.scale, 1)
-      context.scale(scale, scale)
-      contextDraws.scale(scale, scale)
-      context.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0)
-      contextDraws.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0)
+      var transform =
+        outputScale !== 1 ? [ outputScale, 0, 0, outputScale, 0, 0 ] : null;
 
       await page.render({
-        canvasContext: canvas.getContext('2d'),
-        viewport,
-      }).promise
+        canvasContext: canvas.getContext("2d"),
+        transform: transform,
+        viewport: viewport
+      }).promise;
+
+      // canvas.width = draws.width = viewport.width
+      // canvas.height = draws.height = viewport.height
+
+      // const context = canvas.getContext('2d')
+      // const contextDraws = draws.getContext('2d')
+      // let scale = Math.max(this.scale, 1)
+      // context.scale(scale, scale)
+      // contextDraws.scale(scale, scale)
+      // context.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0)
+      // contextDraws.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0)
+
+      // await page.render({
+      //   canvasContext: canvas.getContext("2d"),
+      //   viewport
+      // }).promise;
     },
 
-    updatePage(page, canvas, draws) {
-      console.log('updating page', page)
-      const context = canvas.getContext('2d')
-      const contextDraws = draws.getContext('2d')
-      context.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0)
-      contextDraws.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0)
+    updatePage (page, canvas, draws) {
+      console.log("updating page", page);
+      const context = canvas.getContext("2d");
+      const contextDraws = draws.getContext("2d");
+      context.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0);
+      contextDraws.translate(this.cameraOffsetX ?? 0, this.cameraOffsetY ?? 0);
     },
 
-    handleEvent(event) {
+    handleEvent (event) {
       this.$emit(
-        'canvasEvent',
+        "canvasEvent",
         event,
-        event.target?.attributes?.getNamedItem('page')?.value
-      )
+        event.target?.attributes?.getNamedItem("page")?.value
+      );
     },
     /**
      * Renders the annotation layer for the specified page.
@@ -379,8 +395,8 @@ export default {
      * @param {HTMLElement} container - HTML container.
      * @param {number} width - Actual page width.
      */
-    async renderPageAnnotationLayer(page, container, width) {
-      emptyElement(container)
+    async renderPageAnnotationLayer (page, container, width) {
+      emptyElement(container);
       pdf.AnnotationLayer.render({
         annotations: await page.getAnnotations(),
         div: container,
@@ -390,13 +406,13 @@ export default {
         viewport: page
           .getViewport({
             scale: width / page.view[2],
-            rotation: this.rotation,
+            rotation: this.rotation
           })
           .clone({
-            dontFlip: true,
+            dontFlip: true
           }),
-        imageResourcesPath: this.imageResourcesPath,
-      })
+        imageResourcesPath: this.imageResourcesPath
+      });
     },
     /**
      * Renders the text layer for the specified page.
@@ -404,19 +420,19 @@ export default {
      * @param {HTMLElement} container - HTML container.
      * @param {number} width - Actual page width.
      */
-    async renderPageTextLayer(page, container, width) {
-      emptyElement(container)
+    async renderPageTextLayer (page, container, width) {
+      emptyElement(container);
       await pdf.renderTextLayer({
         container,
         textContent: await page.getTextContent(),
         viewport: page.getViewport({
           scale: width / page.view[2],
-          rotation: this.rotation,
-        }),
-      }).promise
-    },
-  },
-}
+          rotation: this.rotation
+        })
+      }).promise;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
